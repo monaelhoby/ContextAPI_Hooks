0c05911eeed06aa459c6c6502e98cf23c315ed87
mona mohamed <monaelhoby@yahoo.com>
2020-01-19 03:08:47 +0200

    Updates

---
+++ precache-manifest.b9dac7c1c019a08de08c04415e56b001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02ecf99344288f49a70d3e2442b73b3e",
+    "url": "/ContextAPI_Hooks/index.html"
+  },
+  {
+    "revision": "ece8eb11d5b91bca77c4",
+    "url": "/ContextAPI_Hooks/static/css/main.4c6bc57a.chunk.css"
+  },
+  {
+    "revision": "3b50195bccd4ec0518d0",
+    "url": "/ContextAPI_Hooks/static/js/2.94f2a91d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ContextAPI_Hooks/static/js/2.94f2a91d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ece8eb11d5b91bca77c4",
+    "url": "/ContextAPI_Hooks/static/js/main.a4a540ef.chunk.js"
+  },
+  {
+    "revision": "4ae1b2922fa840c62753",
+    "url": "/ContextAPI_Hooks/static/js/runtime-main.69b36950.js"
+  }
+]);
